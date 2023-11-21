d39d4b9ee1624af98879c124cb4f2da4741b6f31
superuser <jeffriver0360@gmail.com>
2023-11-21 16:18:11 +0800

    Complete the test class for addFive

--- src/main/java/tudelft/gettingstarted/GettingStarted.java
+++ src/main/java/tudelft/gettingstarted/GettingStarted.java
@@ -1,7 +1,48 @@
 package tudelft.gettingstarted;
 
+import org.jetbrains.annotations.TestOnly;
+
 public class GettingStarted {
     public int addFive (int number) {
         return number + 5;
     }
+    @TestOnly
+    public void addFiveToTwenty(){
+        int result = new GettingStarted().addFive(20);
+        Assertions.assertEquals(25,result);
+        }
+
+        @TestOnly
+// Add the correct numbers
+    public void addFiveToZero() {
+        int result = new GettingStarted().addFive();
+        Assertions.assertEquals();
+    }
+
+    @TestOnly
+// Add the correct numbers
+    public void addFiveToZero() {
+        int result = new GettingStarted().addFive(0);
+        Assertions.assertEquals(5, result);
+    }
+
+    @TestOnly
+// Write this test
+    public void addFiveToMinusTwenty() {
+
+    }
+
+    @TestOnly
+// Write this test
+    public void addFiveToMinusTwenty() {
+        int result = new GettingStarted().addFive(-20);
+    }
+
+    @TestOnly
+// Write this test
+    public void addFiveToMinusTwenty() {
+        int result = new GettingStarted().addFive(-20);
+        Assertions.assertEquals(-15, result);
+    }
+
 }
